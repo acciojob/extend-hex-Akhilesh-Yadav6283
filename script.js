126ef5c77072eb76055b50372882983f340d7929
Akhilesh-Yadav6283 <135110548+Akhilesh-Yadav6283@users.noreply.github.com>
2023-12-09 00:01:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const extendHex = (shortHex) => {
   // write your code here
+	  shortHex = shortHex.replace('#', '');
+
+  // Split the shortHex into individual color components
+  const red = shortHex.charAt(0);
+  const green = shortHex.charAt(1);
+  const blue = shortHex.charAt(2);
+
+  // Extend each color component to two characters
+  const fullHex =
+    red + red +
+    green + green +
+    blue + blue;
+
+  // Add the '#' symbol and return the full hex code
+  return '#' + fullHex.toUpperCase();
+
 };
 
-// Do not change the code below.
+// Do not change the code below. 
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
